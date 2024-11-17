ade858043b2b5187863697d1617f85b1cb0b983d
bdr-Laptop <coderbadger7@gmail.com>
2024-11-17 21:07:45 +0300

    Unused code snippet deleted

--- 1_proposal-form/proposal-form.js
+++ 1_proposal-form/proposal-form.js
@@ -4,31 +4,23 @@ function addProduct() {
     detailsElement.id = `item-${itemCount}`;
     detailsElement.className = 'accordion';
     detailsElement.setAttribute('name', 'form-detail');
-    itemCount++;
-
-
+    
     // Add inner HTML for the details element
     detailsElement.innerHTML = `
-        <summary>Product Details ${itemCount}</summary>
-        <input class="inp-one" type="text" name="${itemCount}0-prod-code" id="${itemCount}0-prod-code" placeholder="Product Code">
-        <input class="inp-one" type="text" name="${itemCount}1-prod-descr" id="${itemCount}1-prod-descr" placeholder="Description">
-        <input class="inp-one" type="number" name="${itemCount}2-prod-qty" id="${itemCount}2-prod-qty" placeholder="Quantity">
-        <input class="inp-one" type="text" name="${itemCount}3-prod-unit" id="${itemCount}3-prod-unit" placeholder="Unit">
-        <input class="inp-one" type="number" name="${itemCount}4-prod-gross-price" id="${itemCount}4-prod-gross-price" placeholder="Gross Price">
-        <input class="inp-one" type="number" name="${itemCount}5-prod-disc-rate" id="${itemCount}5-prod-disc-rate" placeholder="Discount Rate">
-        <input class="inp-one" type="number" name="${itemCount}6-prod-reduced-price" id="${itemCount}6-prod-reduced-price" placeholder="Reduced Price">
-        <input class="inp-one" type="number" name="${itemCount}7-prod-net-price" id="${itemCount}7-prod-net-price" placeholder="Net Price">
+        <summary>Product Details ${itemCount+1}</summary>
+        <input class="inp-one" type="text" name="${itemCount}-0-prod-code" id="${itemCount}-0-prod-code" placeholder="Product Code">
+        <input class="inp-one" type="text" name="${itemCount}-1-prod-descr" id="${itemCount}-1-prod-descr" placeholder="Description">
+        <input class="inp-one" type="number" name="${itemCount}-2-prod-qty" id="${itemCount}-2-prod-qty" placeholder="Quantity">
+        <input class="inp-one" type="text" name="${itemCount}-3-prod-unit" id="${itemCount}-3-prod-unit" placeholder="Unit">
+        <input class="inp-one" type="number" name="${itemCount}-4-prod-gross-price" id="${itemCount}-4-prod-gross-price" placeholder="Gross Price">
+        <input class="inp-one" type="number" name="${itemCount}-5-prod-disc-rate" id="${itemCount}-5-prod-disc-rate" placeholder="Discount Rate">
+        <input class="inp-one" type="number" name="${itemCount}-6-prod-reduced-price" id="${itemCount}-6-prod-reduced-price" placeholder="Reduced Price">
+        <input class="inp-one" type="number" name="${itemCount}-7-prod-net-price" id="${itemCount}-7-prod-net-price" placeholder="Net Price">
     `;
 
-    // Remove 'hidden' attribute from '.prod-info'
-    const section = document.querySelector('.prod-info');
-    if (section.hasAttribute('hidden')) {
-        section.removeAttribute('hidden');
-        document.getElementById('add-product').style.width = '1.2em';
-    }
-
     // Append the element to the section (class='prod-info') element
-    section.appendChild(detailsElement);
+    document.querySelector('.prod-info').appendChild(detailsElement);
+    itemCount++;
 }
 
 // Select the element with id of 'add-product'
@@ -41,6 +33,6 @@ addProductButton.addEventListener('click', addProduct);
 let itemCount = 0;
 
 // Add initial 5 rows
-for (itemCount; itemCount<5; ) {
+for (itemCount; itemCount<10; ) {
     addProduct();
 }
